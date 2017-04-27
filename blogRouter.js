1439206777623c016fa4d1bb118408f67ad07141
j-Sut <flowstategame@gmail.com>
2017-04-26 20:30:58 -0700

    router functions prepared for export

---
+++ blogRouter.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models.js')
+
+
+// Do I need to add blog entries here just to have something 
+// since we're not working with databases yet? 
+
+
+
+// Get requests on root
+router.get('/', (req, res) => {
+	res.json(BlogPosts.get())
+});
+
+// Post requests on root
+
+router.post('/', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author', 'publishDate'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	res.status(201).json(item);
+});
+
+// Delete requests on root
+router.delete('/:id', (req, res) => {
+	BlogPosts.delete(req.params.id);
+	console.log(`Deleted shopping list item \`${req.params.id}\``);
+	res.status(204).end();
+});
+
+// Put requests on root
+
+router.put('/:id', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author', 'publishDate', 'id'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in requiredFields)) {
+			const message = `Missing \`${field}\` in request body`; 
+			console.error(message);
+			return res.status(400).send(message);
+		}	
+	}
+	if (req.params.id !== req.body.id) {
+		const message = (
+			`Request path id (${req.params.id}) and request body id `
+			`(${req.body.id}) must match`);
+		console.error(message);
+		return res.status(400).send(message)
+	}
+	console.log(`Upadating blog post item \`${req.params.id}\``);
+	const updatedPost = BlogPosts.update({
+		id: req.params.id,
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author,
+		publishDate: req.body.publishDate	
+	});
+	res.status(204).json(updatedPost);
+});
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

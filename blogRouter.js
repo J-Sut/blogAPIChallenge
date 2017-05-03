39d5c162131293c4811f4bb6ed8578cc66319da2
j-Sut <flowstategame@gmail.com>
2017-05-03 08:26:56 -0700

    added travis file, prepped for test integration

--- blogRouter.js
+++ blogRouter.js
@@ -14,7 +14,7 @@ const {BlogPosts} = require('./models.js')
 
 // Get requests on root
 router.get('/', (req, res) => {
-	res.json(BlogPosts.get())
+	res.json(BlogPosts.get());
 });
 
 // Post requests on root
@@ -29,8 +29,8 @@ router.post('/', jsonParser, (req, res) => {
 			return res.status(400).send(message);
 		}
 	}
-	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-	res.status(201).json(item);
+	const post = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	res.status(200).json(post);
 });
 
 // Delete requests on root
@@ -49,7 +49,7 @@ router.put('/:id', jsonParser, (req, res) => {
 		if (!(field in req.body)) {
 			const message = `Missing \`${field}\` in request body`; 
 			console.error(message);
-			return res.status(417).send(message);
+			return res.status(400).send(message);
 		}	
 	}
 	if (req.params.id !== req.body.id) {
@@ -67,7 +67,7 @@ router.put('/:id', jsonParser, (req, res) => {
 		author: req.body.author,
 		publishDate: req.body.publishDate	
 	});
-	res.status(204).json(updatedPost);
+	res.status(201).json(updatedPost);
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -13,10 +13,11 @@ app.use(morgan('common'));
 BlogPosts.create('Harry Potter', 'it was a long book with a lot happening', 'J.K. Rowling', Date.now());
 BlogPosts.create('Mind Clear as a Crystal', 'When the waters of the mind calm, clarity results','J.M. Sutton', Date.now());
 
-
-app.use(express.static('public'));
-
-app.use('/blogRouter', blogRouter);
+app.use('/blog', blogRouter);
+app.use('/blog/comments', commentRouter);
+//GET http://localhost:8080/blog/
+//POST http://localhost:8080/blog/
+//GET http://localhost:8080/blog/comments/
 
 app.listen(process.env.PORT || 8080, () => {
 	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);

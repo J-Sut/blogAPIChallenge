b686ae67f271d6eb9cd37307bd36dfdc4336832d
j-Sut <flowstategame@gmail.com>
2017-05-03 12:29:08 -0700

    redid PUT & POST tests for practice

--- test/blog-test.js
+++ test/blog-test.js
@@ -35,44 +35,90 @@ describe('Blog Api', function() {
 			});
 	});
 
-	it('should add an item on POST', function(){
-		const newPost = {title: "Mind Games", author: "Suzie Q", content: "the key to the mind game is to never forget to ..." };
+	it('should add a new post on POST', function(){
+		const newPost = {title: 'new post title', author: 'cool chick', content: 'is your mind blown yet?'}
 		return chai.request(app)
 			.post('/blog')
 			.send(newPost)
 			.then(function(res){
 				res.should.have.status(200);
 				res.should.be.json;
-				res.body.should.be.a('object');
 				res.body.should.include.keys('title', 'author', 'content', 'publishDate');
 				res.body.id.should.not.be.null;
-				res.body.should.deep.equal(Object.assign(newPost, {id: res.body.id, publishDate: res.body.publishDate}));
+				res.body.should.deep.equal(Object.assign(newPost, {id: res.body.id, publishDate:res.body.publishDate}));
 			});
 	});
 
-	it('should update items on PUT', function(){
-		const updateData = {
-			title: 'Cool stuff',
-			author: 'Jam',
-			content: 'Woah... It totally worked...!',
-			publishDate: Date.now()
-		};
+	it('should update the blog when on PUT', function(){
+	    const updateData = {
+	      title: 'foo',
+	      author: 'some guy',
+	      content: 'good stuff, very well written',
+	      publishDate: Date.now()
+	    };		
 
-		return chai.request(app)
-			.get('/blog')
-			.then(function(res) {
-				updateData.id = res.body[0].id;
-				return chai.request(app)
-					.put(`/blog/${updateData.id}`)
-					.send(updateData);
-			})
-			.then(function(res){
-				res.should.have.status(201);
-				res.should.be.json;
-				res.body.should.be.a('object');
-				res.body.should.deep.equal(updateData);
-			});
+	    return chai.request(app)
+	    	.get('/blog')
+	    	.then(function(res) {
+	    		updateData.id = res.body[0].id;
+	    		return chai.request(app)
+	    			.put(`/blog/${updateData.id}`)
+	    			.send(updateData);
+	    	})
+	    	.then(function(res){
+	    		res.should.have.status(201);
+	    		res.should.be.json;
+	    		res.body.should.be.a('object');
+	    		res.body.should.deep.equal(updateData)
+	    	});
 	});
+
+
+
+
+
+
+
+
+
+	// it('should add an item on POST', function(){
+	// 	const newPost = {title: "Mind Games", author: "Suzie Q", content: "the key to the mind game is to never forget to ..." };
+	// 	return chai.request(app)
+	// 		.post('/blog')
+	// 		.send(newPost)
+	// 		.then(function(res){
+	// 			res.should.have.status(200);
+	// 			res.should.be.json;
+	// 			res.body.should.be.a('object');
+	// 			res.body.should.include.keys('title', 'author', 'content', 'publishDate');
+	// 			res.body.id.should.not.be.null;
+	// 			res.body.should.deep.equal(Object.assign(newPost, {id: res.body.id, publishDate: res.body.publishDate}));
+	// 		});
+	// });
+
+	// it('should update items on PUT', function(){
+	// 	const updateData = {
+	// 		title: 'Cool stuff',
+	// 		author: 'Jam',
+	// 		content: 'Woah... It totally worked...!',
+	// 		publishDate: Date.now()
+	// 	};
+
+	// 	return chai.request(app)
+	// 		.get('/blog')
+	// 		.then(function(res) {
+	// 			updateData.id = res.body[0].id;
+	// 			return chai.request(app)
+	// 				.put(`/blog/${updateData.id}`)
+	// 				.send(updateData);
+	// 		})
+	// 		.then(function(res){
+	// 			res.should.have.status(201);
+	// 			res.should.be.json;
+	// 			res.body.should.be.a('object');
+	// 			res.body.should.deep.equal(updateData);
+	// 		});
+	// });
 
 
 	it('should delete items on DELETE', function(){

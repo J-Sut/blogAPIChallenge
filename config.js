0b8792a44b4b6d0eab386899398b39fce636c116
j-Sut <flowstategame@gmail.com>
2017-05-14 17:22:13 -0700

    moving db to mlab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blogseeddb';
+                      'mongodb://blogApiJam:NPf6vxX7z52ytNHD@ds143191.mlab.com:43191/thinkful-blog-api';
                       
 exports.TEST_DATABASE_URL = (
 						process.env.TEST_DATABASE_URL ||

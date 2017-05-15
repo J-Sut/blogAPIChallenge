a94aa50d5ecd0a3830e610ee35ddbe33a367559a
j-Sut <flowstategame@gmail.com>
2017-05-15 10:46:23 -0700

    updated config for Travis

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/blog-app-api';
                       
-exports.TEST_DATABASE_URL = (
-						process.env.TEST_DATABASE_URL ||
-						'mongodb://localhost/test-blog-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+							'mongodb://localhost/test-blog-app';
+
 
 exports.PORT = process.env.PORT || 8080;
